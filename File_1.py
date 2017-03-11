165ec9c93771dd78481df4d1f35ba7128be1def4
Shashank Kumar <shashank136.sk@gmail.com>
2017-03-11 16:29:34 +0530

    Update File_1.py

--- File_1.py
+++ File_1.py
@@ -4,10 +4,10 @@ import File_2
 key_words = list(string.ascii_letters)
 
 # creating the required CSV file to store the data
-filename = "DETAILS.csv"
+filename = "school_details.csv"
 f = open(filename, "w")
 
-headers = "School_Name, Affiliation_no, State, District, Postal_address, Phone_no, Email_id, Web_site, Year_of_foundation, Date_of_opening, Name_of_Principal, Status_of_affiliation, Type_of_affiliation, Affiliation_from, Affiliation_to, Name_of_trust_society_managing_committee\n"
+headers = "School_Name, Affiliation_no, State, District, Postal_address, Pin_code, Phone_no, Email_id, Web_site, Year_of_foundation, Date_of_opening, Name_of_Principal, Sex, Status_of_affiliation, Type_of_affiliation, Affiliation_from, Affiliation_to, Name_of_trust_society_managing_committee\n"
 
 f.write(headers)
 f.close()
